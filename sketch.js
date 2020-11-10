b23a7a222eeff7c879c06f602f9fb5ef27092eac
aanvi20 <aanvi.saxena2005@gmail.com>
2020-11-10 16:52:54 +0530

    creating constraint

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6
+var sling1
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,9 +36,12 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(640,280,150, PI/2);
+
 
     bird = new Bird(100,100);
-
+    
+    sling1 = new Sling(bird.body, log6.body)
 }
 
 function draw(){
@@ -58,7 +64,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
     platform.display();
+    sling1.display();
 }
